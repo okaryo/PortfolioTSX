2983066c89f576ef58af9de816e5e8122625d18f
okaryo <okaryotr@gmail.com>
2020-10-04 23:35:18 +0900

    [fix] month expression

--- src/utils/dateFormatter.ts
+++ src/utils/dateFormatter.ts
@@ -1,6 +1,6 @@
 export const dateFormatter = (dateString: string) => {
   const monthExpressions = [
-    'Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'Jun', 'July', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.'
+    'Jan.', 'Feb.', 'Mar.', 'Apr.', 'May', 'June', 'July', 'Aug.', 'Sep.', 'Oct.', 'Nov.', 'Dec.'
   ]
 
   const date = new Date(dateString)
