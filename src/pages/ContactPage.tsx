d0893bfd94c3391e3b62b7ea2d42eb84ea6f58cd
okaryo <okaryotr@gmail.com>
2020-10-09 02:27:53 +0900

    [add] page components

---
+++ src/pages/ContactPage.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import Header from '../components/Header'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+
+const ContactPage = () => {
+  return(
+    <div>
+      <Header activePage={'contact'} />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default ContactPage
