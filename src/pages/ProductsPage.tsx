0e5126cd3da19710fb4452c234d3820ea6304c2d
okaryo <okaryotr@gmail.com>
2020-10-17 01:12:53 +0900

    [fix] Footer components by refactoring

--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-
 import Header from '../components/Header'
 import Products from '../components/Products'
-import Footer from '../components/Footer'
+import Footer from '../components/Footer/Footer'
 
 const ProductPage = () => {
   return(
--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-
 import Header from '../components/Header'
 import Profile from '../components/Profile'
-import Footer from '../components/Footer'
+import Footer from '../components/Footer/Footer'
 
 const ProfilePage = () => {
   return(
