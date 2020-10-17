eb705ec8de4ba6b891de042920277dc849c3c25d
okaryo <okaryotr@gmail.com>
2020-10-17 19:03:32 +0900

    [fix] contact button style

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,30 +3,29 @@ import '../stylesheets/Contact.css'
 
 
 const Contact = () => {
-  const inputValue = "Please contact me on Twitter if you need me."
+  const placeholder = "Please contact me on Twitter if you need me."
 
   return(
     <div>
       <div className="headerSpace" />
-      <div className="main_body portfolio_contact_container">
-        <h3>Feel free to contact me!</h3>
-        <p>E-mail</p>
-        <input type="text" defaultValue={inputValue} />
-        <p>Subject</p>
-        <input type="text" defaultValue={inputValue} />
-        <p>Message</p>
-        <textarea rows={10} defaultValue={inputValue}></textarea>
-        <div className="portfolio_contact_button">
-          <button>
-            <a
-              href="https://twitter.com/okaryo_tr"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Go to Twitter
-            </a>
-          </button>
-        </div>
+      <div className="main_body contact_body">
+        <h3 className="contact_mainHeader">Feel free to contact me!</h3>
+        <label className="contact_inputLabel" htmlFor="email">E-mail</label>
+        <input id="email" className="contact_inputArea" type="text" placeholder={placeholder} />
+        <label className="contact_inputLabel" htmlFor="subject">Subject</label>
+        <input id="subject" className="contact_inputArea" type="text" placeholder={placeholder} />
+        <label className="contact_inputLabel" htmlFor="message">Message</label>
+        <textarea id="message" className="contact_inputArea" rows={10} placeholder={placeholder}></textarea>
+        <button className="contact_button">
+          <a
+            className="contact_button-text"
+            href="https://twitter.com/okaryo_tr"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Go to Twitter
+          </a>
+        </button>
       </div>
     </div>
   )
