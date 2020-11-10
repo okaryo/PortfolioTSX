41952beba45e4a0f325b74ada1b3119ac4904ac8
okaryo <okaryotr@gmail.com>
2020-11-11 08:54:52 +0900

    [add] input value validation to contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -5,6 +5,9 @@ const Contact = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [content, setContent] = useState('')
+  const [nameErrorText, setNameErrorText] = useState('')
+  const [emailErrorText, setEmailErrorText] = useState('')
+  const [contentErrorText, setContentErrorText] = useState('')
   const [alreadySent, setAlreadySent] = useState(false)
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
@@ -24,6 +27,8 @@ const Contact = () => {
   }
 
   const handleSubmit = (_: React.FormEvent<HTMLButtonElement>) => {
+    if (isValidateInputValues()) return
+
     const confirmSubmit = window.confirm('Send an email. Is this content correct?')
     if (confirmSubmit) {
       const sendMail = functions.httpsCallable('sendMail')
@@ -33,8 +38,57 @@ const Contact = () => {
         content: content
       }
       sendMail(data)
+      setNameErrorText('')
+      setEmailErrorText('')
+      setContentErrorText('')
       setAlreadySent(true)
     }
+  }
+
+  const isValidateInputValues = () => {
+    let isError = false
+    if (!isValidateNameValue()) isError = true
+    if (!isValidateEmailValue()) isError = true
+    if (!isValidateContentValue()) isError = true
+    return isError
+  }
+
+  const isValidateNameValue = () => {
+    if (name.length === 0) {
+      setNameErrorText('enter your name')
+      return false
+    } else if (name.length > 255) {
+      setNameErrorText('must be 255 characters or less')
+      return false
+    }
+    return true
+  }
+
+  const isValidateEmailValue = () => {
+    const EMAIL_FORMAT = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}\.[A-Za-z0-9]{1,}$/
+
+    if (email.length === 0) {
+      setEmailErrorText('enter your email')
+      return false
+    } else if (email.length > 255) {
+      setEmailErrorText('must be 255 characters or less')
+      return false
+    } else if (!email.match(EMAIL_FORMAT)) {
+      setEmailErrorText('invalid email format')
+      return false
+    }
+    return true
+  }
+
+  const isValidateContentValue = () => {
+    if (content.length === 0) {
+      setContentErrorText('enter content body')
+      return false
+    } else if (content.length > 1000) {
+      setContentErrorText('must be 1000 characters or less')
+      return false
+    }
+    return true
   }
 
   return(
@@ -45,12 +99,15 @@ const Contact = () => {
 
         <label className="contact_inputLabel" htmlFor="name">Name</label>
         <input id="name" name="name" className="contact_inputArea" type="text" value={name} onChange={handleChange} />
+        <p className="contact_error">{nameErrorText}</p>
 
         <label className="contact_inputLabel" htmlFor="email">E-mail</label>
         <input id="email" name="email" className="contact_inputArea" type="text" value={email} onChange={handleChange} />
+        <p className="contact_error">{emailErrorText}</p>
 
         <label className="contact_inputLabel" htmlFor="content">Content</label>
         <textarea id="content" name="content" className="contact_inputArea" rows={10} value={content} onChange={handleChange} ></textarea>
+        <p className="contact_error">{contentErrorText}</p>
 
         <button className="contact_button" type="submit" onClick={handleSubmit} disabled={alreadySent} >
           {alreadySent ? 'Successfully Sent': 'Send'}
