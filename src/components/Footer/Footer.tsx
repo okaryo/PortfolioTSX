d31d7b28a2886c422c1faa0d7a65bc738b43236d
okaryo <okaryotr@gmail.com>
2022-01-18 22:12:17 +0900

    [modify] copyright year to js script

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return(
     <footer className="footer_container">
       <FooterNavigationList navigationItems={FooterNavigationItems} />
-      <p className="footer_copyright">Copyright &copy;2022 OKARYO</p>
+      <p className="footer_copyright">Copyright &copy;{new Date().getFullYear()} OKARYO</p>
       <p className="footer_noteAboutGA">This site uses Google Analytics.</p>
     </footer>
   )
