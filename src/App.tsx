47cd67867802e2e940446f8e92134a46885bcfe8
okaryo <okaryotr@gmail.com>
2022-06-19 17:07:17 +0900

    [modify] import class name

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,10 @@ import {
   Route
 } from 'react-router-dom'
 
-import ProductsPage from './features/Products'
-import ProfilePage from './features/Profile'
-import ContactPage from './features/Contact'
-import NotFoundPage from './features/NotFound'
+import Products from './features/Products'
+import Profile from './features/Profile'
+import Contact from './features/Contact'
+import NotFound from './features/NotFound'
 import Header from './features/Header'
 import Footer from './features/Footer'
 
@@ -16,11 +16,11 @@ const App = () => {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<ProductsPage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route element={<NotFoundPage />} />
+        <Route path="/" element={<Products />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route element={<NotFound />} />
       </Routes>
       <Footer />
     </Router>
